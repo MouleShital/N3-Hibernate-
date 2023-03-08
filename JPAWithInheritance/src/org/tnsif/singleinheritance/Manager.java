113d8e4508c03fb0855d9ebd502281a33b5e188a
Shital <shitalmoule1234@gmail.com>
2023-03-08 14:11:50 +0530

    First Commit

---
+++ JPAWithInheritance/src/org/tnsif/singleinheritance/Manager.java
@@ -0,0 +1,5 @@
+package org.tnsif.singleinheritance;
+
+public class Manager {
+
+}
---
+++ JPAWithInheritance/src/org/tnsif/singleinheritance/SingleTableInheritance.java
@@ -0,0 +1,10 @@
+package org.tnsif.singleinheritance;
+
+public class SingleTableInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
